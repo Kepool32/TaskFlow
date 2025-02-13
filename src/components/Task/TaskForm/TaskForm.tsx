11ecf49ec84dec4136b368f35d4a8843edf9bcb7
Kepool32 <109909825+Kepool32@users.noreply.github.com>
2025-02-13 18:00:35 +0300

    Update priority display

--- src/components/Task/TaskForm/TaskForm.tsx
+++ src/components/Task/TaskForm/TaskForm.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import styles from './TaskForm.module.scss';
+import {Task} from "@types/taskTypes.ts";
 
 interface TaskFormProps {
-  onSubmit: (task: any) => void;
+  onSubmit: (task: Task) => void;
   projectId: string | undefined;
 }
 
 const TaskForm: React.FC<TaskFormProps> = ({ onSubmit, projectId }) => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [priority, setPriority] = useState('Low'); // Значение по умолчанию
+  const [priority, setPriority] = useState('Low');
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -30,25 +31,29 @@ const TaskForm: React.FC<TaskFormProps> = ({ onSubmit, projectId }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.taskForm}>
-      <input
-        type="text"
-        placeholder="Task Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <textarea
-        placeholder="Task Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
-        <option value="Low">Low</option>
-        <option value="Medium">Medium</option>
-        <option value="High">High</option>
-      </select>
-      <button type="submit">Create Task</button>
-    </form>
+      <form onSubmit={handleSubmit} className={styles.taskForm}>
+          <input
+              type="text"
+              placeholder="Task Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+          />
+          <textarea
+              placeholder="Task Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+          />
+          <div className={styles.priorityItem}>
+              <label htmlFor="priority" className={styles.label}>Приоритет:</label>
+              <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+                  <option value="Low">Low</option>
+                  <option value="Medium">Medium</option>
+                  <option value="High">High</option>
+              </select>
+
+          </div>
+          <button type="submit">Create Task</button>
+      </form>
   );
 };
 
